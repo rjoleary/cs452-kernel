714df3fdfaa2a526fab2d3f90803bbac6c361d2b
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-11 16:32:06 -0400

    Print buildstr in main

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,13 +2,18 @@
 #include <td.h>
 #include <bwio.h>
 
+const char* buildstr();
+
 int main() {
+    // Print the build string (date + time).
+    bwputstr(COM2, buildstr());
+    bwputstr(COM2, "\r\nHello World!\r\n");
+
     // Initialize task descriptors.
-    bwputstr(COM2, "This is a test");
     struct Td tds[NUM_TD];
     for (int i = 0; i < NUM_TD; i++) {
         tds[i].tid = -1;
     }
-    
+
     return 0;
 }
