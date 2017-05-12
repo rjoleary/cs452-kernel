343a191d5b17d4ef9153597f2d331c4dac777039
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-11 23:06:29 -0400

    Implementation of memcpy and memmove

---
+++ include/user/std.h
@@ -0,0 +1,7 @@
+#ifndef USER_STD__INCLUDED
+#define USER_STD__INCLUDED
+
+void *memcpy(void *dest, const void *src, unsigned n);
+void *memmove(void *dest, const void *src, unsigned n);
+
+#endif // USER_STD__INCLUDED
---
+++ src/user/std.c
@@ -0,0 +1,9 @@
+#include <user/std.h>
+
+void *memcpy(void *dest, const void *src, unsigned n) {
+    return __builtin_memcpy(dest, src, n);
+}
+
+void *memmove(void *dest, const void *src, unsigned n) {
+    return __builtin_memmove(dest, src, n);
+}
