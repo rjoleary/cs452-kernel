4d5aa5862cde7f10382b3af95d0d9b47f43132f4
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-15 16:11:16 -0400

    Store registers before panic call (to prevent clobber)

--- include/panic.h
+++ include/panic.h
@@ -1,7 +1,18 @@
 #ifndef PANIC_H__INCLUDED
 #define PANIC_H__INCLUDED
 
-#define PANIC(str) panic(str, __FILE__, __LINE__)
-void panic(const char *str, const char *file, int line);
+#define PANIC(str)                        \
+    do {                                  \
+    asm volatile (                        \
+        "stmfd sp!, {r0-r15}"             \
+    );                                    \
+    unsigned *regs;                       \
+    asm volatile (                        \
+        "mov %0, sp"                      \
+        : "=r" (regs)                     \
+    );                                    \
+    panic(regs, str, __FILE__, __LINE__); \
+    } while(0)
+void panic(unsigned *regs, const char *str, const char *file, int line);
 
 #endif // PANIC_H__INCLUDED
