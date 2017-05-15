72e1940797fa56a4107cafddbfa32ee726a7fde7
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-15 15:26:55 -0400

    Document the range of valid priorities

--- include/user/types.h
+++ include/user/types.h
@@ -2,6 +2,10 @@
 #define USER_TYPES_H__INCLUDED
 
 typedef int Tid;
+
+// There are 32 valid priorities: [0,31]
+// 0: low priority
+// 31: high priority
 typedef int Priority;
 
 #endif // USER_TYPES_H__INCLUDED
