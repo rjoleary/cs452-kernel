1d39ce668d7ee1b22e2d01e25d11a1db13d80769
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-23 23:56:26 -0400

    Remove memcpy and memmove

--- include/user/std.h
+++
@@ -1,7 +0,0 @@
-#ifndef USER_STD__INCLUDED
-#define USER_STD__INCLUDED
-
-void *memcpy(void *dest, const void *src, unsigned n);
-void *memmove(void *dest, const void *src, unsigned n);
-
-#endif // USER_STD__INCLUDED
--- src/user/std.c
+++
@@ -1,9 +0,0 @@
-#include <std.h>
-
-void *memcpy(void *dest, const void *src, unsigned n) {
-    return __builtin_memcpy(dest, src, n);
-}
-
-void *memmove(void *dest, const void *src, unsigned n) {
-    return __builtin_memmove(dest, src, n);
-}
