8562bbef4c7a705b9d37be394f9afac4cffaa95c
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-25 19:30:02 -0400

    Call create for nameserver

--- src/first/main.cpp
+++ src/first/main.cpp
@@ -2,17 +2,13 @@
 #include <task.h>
 
 // Forward declaration.
-void testMain();
+namespace ctl {
+void nsMain();
+}
 
-// The first user task performs the following in order:
-//  1. Creates two test tasks of lower priority
-//  2. Creates two test tasks of higher priority
 void firstMain() {
     bwprintf(COM2, "FirstUserTask: entering\r\n");
     bwprintf(COM2, "FirstUserTask: myTid()=%d\r\n", ctl::myTid());
-    bwprintf(COM2, "FirstUserTask: created tid %d\r\n", ctl::create(1, testMain));
-    bwprintf(COM2, "FirstUserTask: created tid %d\r\n", ctl::create(1, testMain));
-    bwprintf(COM2, "FirstUserTask: created tid %d\r\n", ctl::create(5, testMain));
-    bwprintf(COM2, "FirstUserTask: created tid %d\r\n", ctl::create(5, testMain));
+    bwprintf(COM2, "FirstUserTask: created nameserver, tid %d\r\n", ctl::create(1, ctl::nsMain));
     bwprintf(COM2, "FirstUserTask: exiting\r\n");
 }
