7ee6b0c88466bce48c7b273e86b73cccc47e2ebc
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-05-28 01:11:56 -0400

    Add memset

--- include/user/std.h
+++ include/user/std.h
@@ -5,6 +5,7 @@ namespace ctl {
 using size_t = decltype(sizeof(0));
 
 #define memcpy  __builtin_memcpy
+#define memset  __builtin_memset
 #define memcmp  __builtin_memcmp
 #define strncpy __builtin_strncpy
 }
