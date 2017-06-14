14761effc430dc12d6cbf7e35615b5bc9c45dfe5
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-06-14 07:03:20 -0400

    Stop the trains

--- src/first/main.cpp
+++ src/first/main.cpp
@@ -77,6 +77,7 @@ void firstMain() {
         ASSERT(receive(&tid, EmptyMessage) == 0);
     }
 
+    bwputc(COM1, 97);
     // Echo
     Tid io = whoIs(Names::IoServerUart2);
     ASSERT(io.underlying() >= 0);
