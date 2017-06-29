f32848531eb9f0dcef349cc3e9da90617fb3fe22
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-06-28 21:10:14 -0400

    Move declarations into include directory

--- src/track/track.cpp
+++ src/track/track.cpp
@@ -38,32 +38,6 @@ struct Message {
     };
 };
 
-struct Graph {
-    TrackNode *vertices;
-
-    static constexpr size_t VSize = TRACK_MAX;
-
-    auto adjacentN(int idx) const {
-        switch (vertices[idx].type) {
-            case NODE_SENSOR: return 1;
-            case NODE_BRANCH: return 2;
-            case NODE_MERGE: return 1;
-            case NODE_ENTER: return 1;
-            case NODE_EXIT: return 0;
-            default: return 0; // TODO: make enum class
-        }
-    }
-    const TrackEdge *adjacent(int idx, int i) const {
-        return &(vertices[idx].edge[i]);
-    }
-    auto dest(const TrackEdge *edge) const {
-        return edge->dest - vertices;
-    }
-    auto weight(const TrackEdge *edge) const {
-        return edge->dist;
-    }
-};
-
 void sensorNotifierMain() {
     auto serv = whoIs(TrackServName).asValue();
     Message msg = {MsgType::Sensor};
