bd4def63dbcb29cd590a5e72aa0e2d8c1585b82c
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2017-06-28 21:46:39 -0400

    Add sensor class

--- include/user/sensor.h
+++ include/user/sensor.h
@@ -1,7 +1,85 @@
 #pragma once
 
+#include <bwio.h>
+
+using namespace ctl;
+
 constexpr int NUM_SENSOR_MODULES = 5;
 constexpr int NUM_SENSORS_PER_MODULE = 16;
+
+class Sensor {
+    unsigned char module_; // [0-4]
+    unsigned char sensor_; // [0-15]
+
+    static bool isModule(char c) {
+        return ('a' <= c && c <= 'e') || ('A' <= c && c <= 'E');
+    }
+
+    static bool isDigit(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+public:
+    // Range: [0-79]
+    // TODO: use ErrorOr
+    static Sensor fromInt(int value, bool &error) {
+        Sensor s;
+        if (value < 0 || 79 < value) {
+            error = true;
+            return s;
+        }
+        s.module_ = value / 16;
+        s.sensor_ = value % 16;
+        error = false;
+        return s;
+    }
+
+    // Must be: [A-Ea-e]{1-16}
+    // TODO: user ErrorOr
+    static Sensor fromString(const char *str, bool &error) {
+        Sensor s;
+        if (!str[0] || !str[1]) {
+            error = true;
+            return s;
+        }
+        if (isModule(str[0])) {
+            s.module_ = (str[0] & 0x20) - 'a';
+        }
+        if (isDigit(str[1])) {
+            s.sensor_ = str[1] - '0';
+        }
+        if (str[2] && isDigit(str[2])) {
+            s.sensor_ = s.sensor_ * 10 + (str[2] - '0');
+        }
+        if (str[3]) {
+            error = true;
+            return s;
+        }
+        if (s.sensor_ < 1 || 16 < s.sensor_) {
+            error = true;
+            return s;
+        }
+        s.sensor_--;
+        error = false;
+        return s;
+    }
+
+    unsigned char module() const {
+        return module_;
+    }
+
+    unsigned char sensor() const {
+        return sensor_;
+    }
+
+    unsigned char value() const {
+        return module_ * 16 + sensor_;
+    }
+
+    void print(int com) const {
+        bwprintf(com, "%c%d", module_ + 'A', sensor_);
+    }
+};
 
 struct alignas(4) SensorSet {
     // One bit array per module.
@@ -11,6 +89,10 @@ struct alignas(4) SensorSet {
         if (j < 8)
             return values[i] & (1 << (7-j));
         return values[i] & (1 << (15 -j + 8));
+    }
+
+    bool operator()(Sensor s) const {
+        return (*this)(s.module(), s.sensor());
     }
 
     operator bool() const {
